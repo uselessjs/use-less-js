81972491052b9d1b57961e065b9381c34bd234b3
VivekMadurai <vivek.madurai@orangescape.com>
2016-07-31 01:30:47 +0530

    minimal router is added

--- src/useless.js
+++ src/useless.js
@@ -1,80 +1,124 @@
 'use strict';
 
-var useless = (function () {
+var Application = (function (appName, appDependencies) {
 	var api = {
 	  	'constants': function (name, val) {
    			registry.constants[name] = val;
 		},
-		'application': function(name, dependancies){
-			if(Array.isArray(dependancies)) {
-                
-            }
-        },
+		'service': function (name, handler) {
+			var serviceObj = {};
+			handler.apply(serviceObj);
+   			registry.service[name] = serviceObj;
+		},
     	'controller': function(name, handler) {
     		if(Array.isArray(handler)) {
     			var last_index = handler.length-1;
-	            var dependancies = handler.slice(0, -1);
+	            var ctlDepdendencies = handler.slice(0, -1);
 	            if (typeof handler[last_index] === "function") {
-	                registry.controller[name] = handler[last_index];
-	                registry.controller_dependancy[name] =  dependancies;
+	                registry.controller[name] = handler[last_index].apply({}, api.loadCtrlDependencies(ctlDepdendencies));
 	                return;
 	            }
     		} else if(typeof handler === "function") {
-    			registry.controller[name] = handler;
+    			registry.controller[name] = handler.apply({});
     			return;
     		}
     		throw "controller function is not defined";
     	},
-        'loadDependancies' : function(dependancies){
-            var dependancy = [], iter;
-            for (var i in dependancies) {
-            	var dependant = dependancies[i];
-                if (typeof dependency === "string") {
+        'loadCtrlDependencies' : function(ctlDepdendencies){
+            var dependancy = [];
+            for (var i in ctlDepdendencies) {
+            	var dependant = ctlDepdendencies[i];
+                if (typeof dependant === "string") {
                     if (registry.hasOwnProperty(dependant)){
                         dependancy.push(registry[dependant]);
-                    } else if (registry.constants.hasOwnProperty(dependant)) {
-                            dependancy.push(registry.constants[dependant]);
-                    } 
+                    } else if (registry.service.hasOwnProperty(dependant)) {
+                        dependancy.push(registry.service[dependant]);
+                    } else if (registry.appDependency.hasOwnProperty(dependant)) {
+                    	dependancy.push(registry.appDependency[dependant]);
+                    } else {
+                    	throw "Ctrl dependency "+dependant+"is undefined";
+                    }
                 }
             }
             return dependancy;
         },
-    	'routes': function(url, callback) {
-    		if(typeof callback === "function") {
-   				registry.routes[url] = callback;
-   				return;
+        'loadAppDependencies' : function(){
+        		for(var i in appDependencies) {
+        	if(Array.isArray(appDependencies)) {
+        			var dependantAppName = appDependencies[i];
+        			if(UseLess.appRegistry.hasOwnProperty(dependantAppName)) {
+        				registry.appDependency[dependantAppName] = UseLess.appRegistry[dependantAppName]
+        			} else {
+        				throw "Dependant app "+dependantAppName+" is undefined";
+        			}
+        		}
+        	}
+        },
+    	'route': function(url, mapping) {
+    		if(typeof mapping === "object") {
+   				registry.route[url] = new Router(mapping);
+   				return this;
    			} 
-   			throw "route callback is not defined";
+   			throw "route mapping is not defined";
     	}
 	},
 	router = {
 		'match': function(url){
-			if (registry.routes.hasOwnProperty(url)) {
-				registry.routes[url]();
+			if (registry.route.hasOwnProperty(url)) {
+				registry.route[url].render();
 			} else {
-				console.log("No matching routes found")
+				console.log("No matching route found")
 			}
 		}
 	},
 	registry = {
-      	'constants' : {},
-      	'application' : {},
-      	'controller' : {},'controller_dependancy': {},
-     	'routes' : {}
+		'constants': {},
+      	'service' : {},
+      	'controller' : {},
+      	'appDependency': {},
+     	'route' : {}
 	},
 	init = function(){
+		api.loadAppDependencies();
 	  	window.onhashchange = function () {
-        		router.match(window.location.hash.split('#')[1]);
+        	router.match(window.location.hash.split('#')[1]);
     	}
 	}
 	init();
 
     return {
     	'constants': api.constants,
-      	'application': api.application,
+    	'service': api.service,
       	'controller': api.controller,
-      	'routes': api.routes
+      	'route': api.route
     }
+});
+
+var Router = (function(mapping){
+	var api = {
+		'render': function() {
+			alert(mapping.template);
+		}
+	}
+	return {
+		render: api.render
+	}
+});
+
+var UseLess = (function(){
+	var api = {
+		'create': function(name, dependencies) {
+			var app = new Application(name, dependencies);
+			registry[name] = app;
+			return app;
+		}
+	},
+	registry = {}
+
+	return {
+		'create': api.create,
+		'appRegistry': registry
+	};
 })();
 
 
